b81161b30b0179e0c38c8b591b40ea4cda8c394c
dotkit <ewprjkt@proton.me>
2024-05-11 08:45:05 +0000

    twrp: Add brightness path
    Change-Id: Id01ad24b54bbeead137b030927c9e503550c0c64

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,8 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_EXCLUDE_TWRPAPP := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
 
 # TWRP_EVENT_LOGGING := true
 TWRP_INCLUDE_LOGCAT := true
